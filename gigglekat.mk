5afbdf4c381449a1a3f5ad3217b0260ac8a3b14a
GiggleKat <gigglekatrom@gmail.com>
2014-10-28 18:34:29 -0700

    made giggly

--- gigglekat.mk
+++ gigglekat.mk
@@ -6,12 +6,12 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, device/lge/ls980/ls980.mk)
 
 # phone
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+$(call inherit-product, vendor/gigglekat/config/common_phone.mk)
 
 ## product
 PRODUCT_DEVICE := ls980
 PRODUCT_BRAND := LGE
-PRODUCT_NAME := liquid_ls980
+PRODUCT_NAME := gigglekat_ls980
 PRODUCT_MODEL := LG-LS980
 PRODUCT_MANUFACTURER := lge
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.ls980.$(shell date +%m%d%y).$(shell date +%H%M%S)
